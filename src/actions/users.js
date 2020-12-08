e0e198648b70d98f8501aed7496c3a2890bec8aa
OdongAlican <sandieo.2020@gmail.com>
2020-12-08 22:24:07 +0300

    update users actions

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,8 +1,13 @@
-import { FetchUserRequest, DeleteUserRequest, UpdateUserRequest } from '../utils/api'
+import { FetchUserRequest, 
+    DeleteUserRequest, 
+    UpdateUserRequest,
+    FetchVehicleByUserRequest } from '../utils/api'
 
 export const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
 export const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE'
 export const DELETE_USER = 'DELETE_USER';
+export const FETCH_USERS_VEHICLES_SUCCESS = 'FETCH_USERS_VEHICLES_SUCCESS'
+export const FETCH_USERS_VEHICLES_FAILURE = 'FETCH_USERS_VEHICLES_FAILURE'
 
 export const fetchUsersSuccess = users => ({
     type: FETCH_USERS_SUCCESS,
@@ -17,7 +22,17 @@ export const fetchUsersFailure = error => ({
 export const deleteRequest = id => ({
     type: DELETE_USER,
     payload: id,
-  });
+});
+
+export const fetchUsersVehiclesSuccesss = vehicles => ({
+    type: FETCH_USERS_VEHICLES_SUCCESS,
+    payload: vehicles
+})
+
+export const fetchUsersVehiclesFailure = error => ({
+    type: FETCH_USERS_VEHICLES_FAILURE,
+    payload: error
+})
 
 export const fetchUsers = () => async dispatch => {
     const method = 'get';
@@ -26,7 +41,7 @@ export const fetchUsers = () => async dispatch => {
         const result = response.data;
         dispatch(fetchUsersSuccess(result.users));
     } catch (error) {
-        dispatch(fetchUsersFailure(error));
+        dispatch(fetchUsersFailure(error.message));
     }
 };
 
@@ -36,22 +51,28 @@ export const deleteUser = id => async dispatch => {
         await DeleteUserRequest(method, id);
         dispatch(deleteRequest(id))
     } catch (error) {
-        dispatch(fetchUsersFailure(error));
+        dispatch(fetchUsersFailure(error.message));
     }
 }
 
-export const editUser = ({ first_name, 
-                        last_name, 
-                        user_name, 
-                        phone_number,
-                        password, 
-                        email }) => async dispatch => {
+export const editUser = ({ first_name, last_name, user_name, phone_number, password, email }, 
+            userId, history) => async dispatch => {
     const data = { user: {first_name, last_name, user_name,phone_number,password, email }};
     const method = "put";
     try {
-        await UpdateUserRequest(method,data);
-        // dispatch(updateRequest(id))
+        await UpdateUserRequest(method, data, userId);
+        history.push('/users')
     } catch (error) {
-        
+        dispatch(fetchUsersVehiclesFailure(error.message))
+    }
+}
+
+export const showVehicleByUser = (id) => async dispatch => {
+    const method = "get"
+    try {
+        const response = await FetchVehicleByUserRequest(method,id);
+        dispatch(fetchUsersVehiclesSuccesss(response.data.vehicle))
+    } catch (error) {
+        dispatch(fetchUsersVehiclesFailure(error.message))
     }
 }
