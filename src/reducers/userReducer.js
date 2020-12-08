4d230194b9558f1d5e6583a10108896c6de580f4
OdongAlican <sandieo.2020@gmail.com>
2020-12-08 22:25:57 +0300

    update userReducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,8 +1,12 @@
-import { FETCH_USERS_SUCCESS, FETCH_USERS_FAILURE, DELETE_USER } from '../actions/users';
+import { FETCH_USERS_SUCCESS, 
+    FETCH_USERS_FAILURE, 
+    DELETE_USER,
+    FETCH_USERS_VEHICLES_SUCCESS } from '../actions/users';
 
 const initialState = {
     users: [],
-    error: ''
+    error: '',
+    usersVehicles: []
 }
 
 const usersReducer = (state = initialState, action)=> {
@@ -12,18 +16,28 @@ const usersReducer = (state = initialState, action)=> {
                 ...state,
                 users: action.payload,
                 error: '',
+                usersVehicles: []
             };
         case FETCH_USERS_FAILURE:
             return {
                 ...state,
                 users: [],
                 error: action.payload,
+                usersVehicles: []
+            };
+        case FETCH_USERS_VEHICLES_SUCCESS:
+            return {
+                ...state,
+                users: [],
+                error: '',
+                usersVehicles: action.payload
             };
         case DELETE_USER:
             return {
                 ...state,
                 users: state.users.filter(user => user.id !== action.payload),
                 error: '',
+                usersVehicles: []
             };
         default:
             return state;
