eb86a5cc93fcac1e25545e64b3b35e7f7a9aff36
OdongAlican <sandieo.2020@gmail.com>
2020-12-09 15:03:10 +0300

    add Footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => (
+    <div className="footer-section">
+        <div className="copy-right-section">
+            Copyright &copy; 2020
+        </div>
+        <div className="empty-footer-section"></div>
+        <div className="designed-by-section">
+            <span>Designed and developed by <em>GoMint.net</em></span>
+        </div>
+    </div>
+)
+
+export default Footer;
