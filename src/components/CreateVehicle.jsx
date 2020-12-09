d8f405db804fead19fae513a0faa47c1b6265c2e
OdongAlican <sandieo.2020@gmail.com>
2020-12-10 00:44:23 +0300

    update create vehicle component

--- src/components/CreateVehicle.jsx
+++ src/components/CreateVehicle.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { useDispatch } from "react-redux";
 import { createVehicle } from '../actions/vehicle'
 
-const CreateVehicle = ({ history }) => {
+const CreateVehicle = ({ history, hideCreateVeh, removeCreate }) => {
     const dispatch = useDispatch()
 
     const [chassis, setChasis] = useState('')
@@ -17,52 +17,61 @@ const CreateVehicle = ({ history }) => {
       };
 
     return(
-        <div>
-        <form onSubmit={submitVehicle}>
-            <input
-              type="text"
-              placeholder="Enter Chasis Number"
-              value={chassis}
-              onChange={e => setChasis(e.target.value)}
-              className="input-control mb-2"
-            />
-            <input
-              type="text"
-              placeholder="Enter Model"
-              value={model}
-              onChange={e => setModel(e.target.value)}
-              className="input-control"
-            />
-            <input
-              type="text"
-              placeholder="Enter Title"
-              value={title}
-              onChange={e => setTitle(e.target.value)}
-              className="input-control"
-            />
-            <input
-              type="date"
-              placeholder="Enter Title"
-              value={manufacturing_date}
-              onChange={e => setDate(e.target.value)}
-              className="input-control"
-            />
-            <input
-              type="text"
-              placeholder="Enter Avatar"
-              value={avatar}
-              onChange={e => setAvatar(e.target.value)}
-              className="input-control"
-            />
-            {/* <input
-              type="file"
-              className="mt-2 btn-control file-section"
-              onChange={e => setAvatar(e.target.files[0])}
-            /> */}
-            <button type="submit" className="btn-control mt-1">
-              Create
-            </button>
-          </form>
+        <div className={ `create-vehicle-main ${hideCreateVeh}` }>
+          <div className="inner-create-vehicle-div">
+            <div className="close-div-icon">
+              <i 
+                onClick = { removeCreate }
+              className="fas fa-times-circle"></i>
+            </div>
+            <h6>Add Vehicle</h6>
+            <span>For GoMint</span>
+            <form onSubmit={submitVehicle}>
+                <input
+                  type="text"
+                  placeholder="Enter Chasis Number"
+                  value={chassis}
+                  onChange={e => setChasis(e.target.value)}
+                  className="input-control mb-2"
+                />
+                <input
+                  type="text"
+                  placeholder="Enter Model"
+                  value={model}
+                  onChange={e => setModel(e.target.value)}
+                  className="input-control"
+                />
+                <input
+                  type="text"
+                  placeholder="Enter Title"
+                  value={title}
+                  onChange={e => setTitle(e.target.value)}
+                  className="input-control"
+                />
+                <input
+                  type="date"
+                  placeholder="Enter Title"
+                  value={manufacturing_date}
+                  onChange={e => setDate(e.target.value)}
+                  className="input-control"
+                />
+                <input
+                  type="text"
+                  placeholder="Enter Avatar"
+                  value={avatar}
+                  onChange={e => setAvatar(e.target.value)}
+                  className="input-control"
+                />
+                {/* <input
+                  type="file"
+                  className="mt-2 btn-control file-section"
+                  onChange={e => setAvatar(e.target.files[0])}
+                /> */}
+                <button type="submit" className="btn-control mt-1">
+                  Create
+                </button>
+              </form>            
+            </div>
         </div>
     )
 }
