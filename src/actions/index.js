3b46b2d1ba09610263fbda9326f267b90169438c
OdongAlican <sandieo.2020@gmail.com>
2020-12-10 05:02:12 +0300

    update login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import { sendUnauthenticatedRequest } from '../utils/api';
+import axios from 'axios'
 
 export const AUTHENTICATED = 'authenticated_user';
 export const UNAUTHENTICATED = 'unauthenticated_user';
@@ -8,13 +9,17 @@ export function signInAction({ email, password }, history) {
   return async dispatch => {
     const path = 'users/sign_in';
     const method = 'post';
+
     const data = { user: { email, password }};
+
     try {
       const response = await sendUnauthenticatedRequest(method, path, data);
       dispatch({ type: AUTHENTICATED });
+      localStorage.setItem('current_user', response.data.user_details.id)
       localStorage.setItem('user', response.data.user_details.authentication_token);
-      history.push(`/users/${response.data.user.id}`);
+      history.push(`/users/${response.data.user_details.id}`);
     } catch (error) {
+      console.log(error.message)
       dispatch({
         type: AUTHENTICATION_ERROR,
         payload: 'Invalid username or Password',
