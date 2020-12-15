b993cec711ebaa374c61de7c88f5888cc033c593
OdongAlican <sandieo.2020@gmail.com>
2020-12-15 13:52:08 +0300

    add upload images

--- src/components/CreateVehicle.jsx
+++ src/components/CreateVehicle.jsx
@@ -1,20 +1,44 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useCallback } from 'react'
 import { useDispatch } from "react-redux";
 import { createVehicle } from '../actions/vehicle'
+import Webcam from "react-webcam";
 
 const CreateVehicle = ({ hideCreateVeh, removeCreate, userId }) => {
     const dispatch = useDispatch()
+    const webcamRef = useRef(null)
 
     const [chassis, setChasis] = useState('')
     const [ model, setModel ] = useState('')
     const [ title, setTitle] = useState('')
     const [manufacturing_date, setDate] = useState('')
     const [avatar, setAvatar] = useState('')
+    const [webcamEnabled, setWebcamEnabled] = useState(false)
 
     const submitVehicle = event => {
-        dispatch(createVehicle({chassis,model,title,manufacturing_date,avatar}, userId));
+        dispatch(createVehicle({chassis, model, title, manufacturing_date, avatar}, userId));
         event.preventDefault();
       };
+
+    const enableWebcam = () => {
+      setWebcamEnabled(true)
+    }
+
+    const capture = useCallback(
+      () => {
+      setAvatar('')
+      const imageSrc = webcamRef.current.getScreenshot();
+      setAvatar(imageSrc)
+      setWebcamEnabled(false)
+      },
+      [webcamRef]
+    );
+
+    const uploadImg = (event) => {
+      setAvatar('')
+      setAvatar(event.target.files[0])
+    }
+
+    console.log(avatar)
 
     return(
         <div className={ `create-vehicle-main ${hideCreateVeh}` }>
@@ -55,17 +79,46 @@ const CreateVehicle = ({ hideCreateVeh, removeCreate, userId }) => {
                   onChange={e => setDate(e.target.value)}
                   className="input-control"
                 />
-                <input
-                  // type="file" 
-                  // accept="image/*" 
-                  // capture="environment"
-                  type="file" 
-                  accept="image/*;capture=camera"
-                  placeholder="Enter Avatar"
-                  // value={avatar}
-                  // onChange={e => setAvatar(e.target.value)}
-                  className="input-control"
-                />
+                <div className="upload-capture-image-section">
+                  <div className="image-camera-section-one">
+                    {
+                      avatar.length === 0 ? (
+                      <span>Upload Image.....</span>
+                      ) : avatar.length > 0 && typeof avatar === 'string' ? (
+                      <span>image Uploaded </span>
+                      ) : ( 
+                      <span>{avatar.name}</span>
+                      )
+                    }
+                  <i class="fas fa-paperclip"></i>
+                    <input
+                      type="file"
+                      className="file-input-hidden"
+                      onChange={e => uploadImg(e)}
+                    />
+                  </div>
+                {
+                  webcamEnabled ? (
+                    <div className="main-webcam-division">
+                      <div className="webcam-display-section">
+                      <Webcam
+                        audio={false}
+                        ref={webcamRef}
+                        screenshotFormat="image/jpeg"
+                        className="webcam-display-section-image"
+                        /> 
+                        <div>
+                          <button onClick={capture}>Capture photo</button>
+                        </div>
+                        </div>
+                    </div>
+                      ) : (
+                          <i 
+                          onClick={enableWebcam}
+                          class="fas fa-camera"></i>
+                      )
+                    }
+                </div>
                 <button 
                 onClick={ removeCreate }
                 type="submit" className="btn-control mt-1">
